a32bb8326f81802fe9b2c65b236c176d1a02ae71
Lilo <59840908+LilasCorner@users.noreply.github.com>
2021-09-22 18:57:50 -0500

    bug fix if user tries to reply to comment with no post selected

--- Form1.cs
+++ Form1.cs
@@ -670,7 +670,7 @@ namespace wolfPack_Assign2
         //FIX LATER NEED DOC BOX
         private void addReplyButton_Click(object sender, EventArgs e)
         {
-            if(addReplyTextBox.Text == "" || postListBox.SelectedIndex == -1 )
+            if(addReplyTextBox.Text == "" || postListBox.SelectedIndex == -1 || postListBox.Items[postListBox.SelectedIndex].ToString() == "Wow, such empty!")
 
             {
                 sysOutputTextBox.AppendText("Please type the reply you would like to add and select either a post or comment");
